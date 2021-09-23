d2f1256ea334d1dcaf665bf168d0b1cb7c9bf891
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2021-09-23 05:47:05 -0700

    setting up  backend

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
             <Home/> 
             {/* Landing Page */}
             {/* Login and Registration */}
-
+            
 
             {/* Admin */}
             {/* Customer */}
