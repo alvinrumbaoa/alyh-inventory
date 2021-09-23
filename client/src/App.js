9a7ac60ccf83984829eea771fcf1d19dc789298a
Maica Rumbaoa <maicarumbaoa@Maicas-MacBook-Pro.local>
2021-09-23 05:43:21 -0700

    planning

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { Switch, Route, } from "react-router-dom";
+import Home from "./views/Home"
+
 
 function App() {
   return (
     <div className="App">
         <header className="App-header">
-       
-      </header>
+            <Home/> 
+            {/* Landing Page */}
+            {/* Login and Registration */}
+
+
+            {/* Admin */}
+            {/* Customer */}
+            {/* Item Inquiry */}
+            {/* Order */} 
+            {/* Cart  */}
+        </header>
     </div>
   );
 }
