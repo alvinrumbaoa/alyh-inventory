0bfff0091f67bd57f8c14ad7d6ba3030b780959b
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2021-10-14 23:06:24 -0700

    dashboard

---
+++ client/src/dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Dashboard() {
+	return (
+		<div className="dashboard">
+			{/* Chart Sales */}
+			
+
+			{/* Orders */}
+			{/* Deliveries */}
+			{/* Customer Inquiry */}
+			{/* Item Inquiry */}
+		</div>
+	)
+}
