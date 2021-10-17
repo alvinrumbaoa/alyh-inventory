19bee2c4a7e96463e216b575e009ff1764697434
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2021-10-17 00:22:08 -0700

    save

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navigation  from './Navigation'
+import '../styles/dashboard/Dashboard.css'
 
 export default function Dashboard() {
 	return (
 		<div className="dashboard-container">
+			<Navigation/>
 			{/* Chart Sales */}
 			<h1>Welcome back , username </h1>
 			{/* Reports */}
---
+++ client/src/dashboard/Navigation.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+const Navigation  = () => {
+	return (
+		<div className="dash-navigation">
+			<ul>
+				<li></li>
+			</ul>
+		</div>
+	)
+}
+
+
+
+export default Navigation;
